8a2ad4ca46e5783535eb47dafc7b685787c92293
Mridul <royshuvo87@gmail.com>
2024-04-19 21:39:42 +0600

    1 hour 42  minutes/for and while loops

---
+++ part-9&10-for_while_loops.php
@@ -0,0 +1,13 @@
+<?php
+    for($i = 0; $i < 5; $i++){
+        echo $i . "<br>";
+    }
+?>
+
+<?php
+    $counter = 0;
+    while($counter <= 10){
+        $counter++;
+        echo $counter . "<br>";
+    }
+?>
